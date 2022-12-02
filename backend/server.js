e2d2ab09d6d3d9abef1b75d41b803804329d4629
Tyler Kozlowski <Tyler.J.Kozlowski@gmail.com>
2022-12-01 21:40:20 -0600

    fixed up server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
+require('colors')
+require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
@@ -11,28 +11,24 @@ connectDB()
 
 const app = express()
 
-// Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support Desk API' })
-})
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-// Serve frontend
+// Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
+  // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  )
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
 } else {
-  app.get('/', (req, res) => {
+  app.get('/', (_, res) => {
     res.status(200).json({ message: 'Welcome to the Support Desk API' })
   })
 }
